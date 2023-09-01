07f07ce573629c40b0303555d176bed11c92b83b
Abhishekisback <Abhishekisback@gmail.com>
2023-09-01 17:10:59 +0530

    second* commit

--- src/app/main/[category_wise_products]/[Individualproduct]/page.js
+++ src/app/main/[category_wise_products]/[Individualproduct]/page.js
@@ -1,90 +1,12 @@
-"use client";
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import Image from "next/image";
-import style from "./style.module.css";
-import localfont from "next/font/local";
-import image2 from "../../../../../public/Images/sad.png";
+"use client"
+import React from 'react'
 
-const gfont = localfont({ src: "../../../fonts/Pacifico-Regular.ttf" });
-
-const IndividualproductPage = ({ params }) => {
-  console.log("params2", params.Individualproduct);
-  // const [products, setproducts] = useState({});
-
-  // useEffect(() => {
-  //   loadproductdetails();
-  // }, [loadproductdetails]);
-
-  // async function loadproductdetails() {
-  //   await axios
-  //     .get(
-  //       `https://zaxbys-strapi.onrender.com/api/categorywiseproductlists?filters[Productname][$eq]=${params.Individualproduct}`
-  //     )
-  //     .then((res) => {
-  //       if (res.status === 200 && res.data) {
-  //         console.log("pname", res?.data?.data[0]?.attributes?.Price);
-  //         setproducts(res?.data?.data[0]?.attributes);
-  //       }
-  //     })
-  //     .catch((err) => {
-  //       console.log(err);
-  //     });
-  // }
-  console.log("products state", products?.Price);
-
+function page({params}) {
   return (
-    <>
-      {/* {products?.imageurl && products?.Productname ? (
-        <>
-          <p style={gfont.style} className={style.productname1}>
-            {products?.Productname}
-          </p>
-          <div className={style.mainwrapper}>
-            <div className={style.singleproductcard}>
-              <div>
-                <Image
-                  src={products?.imageurl}
-                  width={350}
-                  height={300}
-                  alt="img"
-                />
-                <p style={gfont.style} className={style.productname}>
-                  {products?.Productname}
-                </p>
-              </div>
-              <div className={style.descwrapper}>
-                <p style={gfont.style} className={style.description}>
-                  {products?.description}
-                </p>
-                <p style={gfont.style} className={style.price}>
-                  Price:{products?.Price} $
-                </p>
+    <div>
+      <p>name{params.Individualproduct}</p>
+    </div>
+  )
+}
 
-                <button
-                  style={gfont.style}
-                  className={style.buynow}
-                  onClick={() => {
-                    alert("Out Of Stock ... will be available soon");
-                  }}
-                >
-                  Buy NOW!!
-                </button>
-              </div>
-            </div>
-          </div>
-        </>
-      ) : (
-        <p className={style.notfound} style={gfont.style}>
-          Oops...Currently
-          <Image src={image2} width={50} height={50} alt="img" />
-          <span style={{ color: "red" }}>{params.Individualproduct}</span> not
-          Found.... .Please Try Reloading
-        </p>
-      )} */}
-      <p>helloooo</p>
-    </>
-  );
-};
-
-export default IndividualproductPage;
+export default page
