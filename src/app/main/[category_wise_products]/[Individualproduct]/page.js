c8e88e1c845d08544379d33a1fc313295d457c20
Abhishekisback <Abhishekisback@gmail.com>
2023-09-04 17:52:15 +0530

    details page commit

--- src/app/main/[category_wise_products]/[Individualproduct]/page.js
+++ src/app/main/[category_wise_products]/[Individualproduct]/page.js
@@ -1,52 +1,22 @@
-"use client";
-import { MakeApiCall } from "@/app/MakeAPICall";
-import style from "./style.module.css";
-import React, {useState, useEffect } from "react";
+"use clients";
 import axios from "axios";
+import React, { useState, useEffect } from "react";
 
 function page({ params }) {
-  console.log("individual product", params.Individualproduct);
-  let url = `https://zaxbys-strapi.onrender.com/api/categorywiseproductlists?filters[Productname][$eq]=${params.Individualproduct}`;
-
-
   useEffect(() => {
-    loadproducts();
-  }, []);
-
-  const [products,setproducts]=useState({})
-
-  function loadproducts() {
     axios
-      .get(url)
-      .then((res) => {
-        if (res.status === 200) {
-          console.log(res.data);
-          setproducts(res.data);
-        }
+      .get("https://fakestoreapi.com/products")
+      .then(() => {
+        console.log(res.data);
       })
-      .catch(() => {});
-  }
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
   return (
     <div>
-      <p>Individual product{params.Individualproduct}</p>
-      {
-        products ?(
-          <>
-          <p>hdhvchvc</p>
-          </>
-
-        ):(
-          <>
-          <p>noo</p>
-          </>
-        )
-      }
-
-      <div className={style.productcontainer}>
-        <div></div>
-        <div></div>
-      </div>
+      <button>hello</button>
     </div>
   );
 }
