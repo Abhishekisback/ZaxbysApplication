0254032d993014a05f620429c9d35522845795b0
Abhishekisback <Abhishekisback@gmail.com>
2023-09-04 17:56:25 +0530

    details page commit

--- src/app/main/[category_wise_products]/[Individualproduct]/page.js
+++ src/app/main/[category_wise_products]/[Individualproduct]/page.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 import React, { useState, useEffect } from "react";
 
-function page({ params }) {
+function Page({ params }) {
   useEffect(() => {
     axios
       .get("https://fakestoreapi.com/products")
@@ -21,4 +21,4 @@ function page({ params }) {
   );
 }
 
-export default page;
+export default Page;
