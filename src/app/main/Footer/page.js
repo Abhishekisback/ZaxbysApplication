cd963834acb481aaff853de14e5ea733a0802d30
Abhishekisback <Abhishekisback@gmail.com>
2023-09-14 19:19:40 +0530

    final commit

--- src/app/main/Footer/page.js
+++ src/app/main/Footer/page.js
@@ -36,14 +36,15 @@ const Footer = () => {
           <button
             className={style.confirmbtn}
             onClick={() => {
-                toast.dismiss()
-                const newWindow = window.open(
-                    "https://www.linkedin.com/in/abhishek-y-88615b216/",
-                    "_blank" // This opens the link in a new window or tab
-                  );
-                  if (newWindow) {
-                    newWindow.focus(); // Focus on the new window/tab
-                  }}}
+              toast.dismiss();
+              const newWindow = window.open(
+                "https://www.linkedin.com/in/abhishek-y-88615b216/",
+                "_blank" // This opens the link in a new window or tab
+              );
+              if (newWindow) {
+                newWindow.focus(); // Focus on the new window/tab
+              }
+            }}
           >
             Confirm
           </button>
@@ -67,102 +68,110 @@ const Footer = () => {
   }
 
   return (
-    <div className={style.footersection}>
-      <ToastContainer />
-      <div
-        style={{
-          ...gfont.style,
-          width: "400px",
-          textAlign: "justify",
-          marginTop: "30px",
-        }}
-      >
-        <div>
-          <h1> About Us </h1>
-        </div>
-        <br />
-        <div>
-          Zaxby&apos;s is known for its diverse menu featuring various chicken
-          options. Their signature items include hand-breaded chicken fingers,
-          traditional and boneless chicken wings, chicken sandwiches, and
-          salads. Customers can choose from a range of sauces and dressings to
-          customize their orders. Zaxby&apos;s offers a unique Southern flavor
-          experience, with a mix of savory and spicy flavors that cater to a
-          wide range of tastes. The food is often described as flavorful and
-          satisfying.
-        </div>
-      </div>
-
-      <div className={style.quicklinks}>
-        <div>
-          <h1 style={gfont.style}>Quick Links</h1>
+    <div style={{backgroundColor:"#E65100"}}>
+      <div className={style.footersection} >
+        <ToastContainer />
+        <div
+          style={{
+            ...gfont.style,
+            width: "400px",
+            textAlign: "justify",
+            marginTop: "30px",
+          }}
+        >
+          <div>
+            <h1> About Us </h1>
+          </div>
+          <br />
+          <div>
+            Zaxby&apos;s is known for its diverse menu featuring various chicken
+            options. Their signature items include hand-breaded chicken fingers,
+            traditional and boneless chicken wings, chicken sandwiches, and
+            salads. Customers can choose from a range of sauces and dressings to
+            customize their orders. Zaxby&apos;s offers a unique Southern flavor
+            experience, with a mix of savory and spicy flavors that cater to a
+            wide range of tastes. The food is often described as flavorful and
+            satisfying.
+          </div>
         </div>
 
-        <div style={{ display: "flex", flexDirection: "column", gap: "20px" }}>
-          <a className={style.links} href="">
-            FAQ&apos;s
-          </a>
-          <a className={style.links} href="">
-            Terms and Conditions
-          </a>
-          <a className={style.links} href="">
-            Privacy Policy
-          </a>
-        </div>
+        <div className={style.quicklinks}>
+          <div>
+            <h1 style={gfont.style}>Quick Links</h1>
+          </div>
 
-        <p style={{ ...gfont.style, fontSize: "20px" }}>
-          Follow us On Social Media
-        </p>
-        <div className={style.sociallinks}>
-          <FaFacebookSquare
-            className={style.icons}
-            onClick={ShowSiteleaving}
-            color="black"
-            size={30}
-          ></FaFacebookSquare>
+          <div
+            style={{ display: "flex", flexDirection: "column", gap: "20px" }}
+          >
+            <a style={{color:"#0c2340"}} className={style.links} href="">
+              FAQ&apos;s
+            </a>
+            <a style={{color:"#0c2340"}} className={style.links} href="">
+              Terms and Conditions
+            </a>
+            <a style={{color:"#0c2340"}} className={style.links} href="">
+              Privacy Policy
+            </a>
+          </div>
 
-          <FaInstagram
-            className={style.icons}
-            onClick={ShowSiteleaving}
-            color="black"
-            size={30}
-          ></FaInstagram>
-
-          <FaTwitter
-            className={style.icons}
-            onClick={ShowSiteleaving}
-            color="black"
-            size={30}
-          ></FaTwitter>
-
-          <FaYoutube
-            className={style.icons}
-            onClick={ShowSiteleaving}
-            color="black"
-            size={30}
-          ></FaYoutube>
-
-          <FaLinkedin
-            className={style.icons}
-            color="black"
-            onClick={ShowSiteleaving}
-            size={30}
-          ></FaLinkedin>
-        </div>
-      </div>
-
-      <div className={style.contactsec}>
-        <div>
-          <h2 style={gfont.style}>Contact Us</h2>
-        </div>
-        <div>
-          <p>Ph : +91 9611346606</p>
-          <br></br>
-          <p>
-            Email:
-            <a href="mailto:26abhishek.y@gmail.com">26abhishek.y@gmail.com</a>
+          <p style={{ ...gfont.style, fontSize: "20px" }}>
+            Follow us On Social Media
           </p>
+          <div className={style.sociallinks}>
+            <FaFacebookSquare
+              className={style.icons}
+              onClick={ShowSiteleaving}
+              color="black"
+              size={30}
+            ></FaFacebookSquare>
+
+            <FaInstagram
+              className={style.icons}
+              onClick={ShowSiteleaving}
+              color="black"
+              size={30}
+            ></FaInstagram>
+
+            <FaTwitter
+              className={style.icons}
+              onClick={ShowSiteleaving}
+              color="black"
+              size={30}
+            ></FaTwitter>
+
+            <FaYoutube
+              className={style.icons}
+              onClick={ShowSiteleaving}
+              color="black"
+              size={30}
+            ></FaYoutube>
+
+            <FaLinkedin
+              className={style.icons}
+              color="black"
+              onClick={ShowSiteleaving}
+              size={30}
+            ></FaLinkedin>
+          </div>
         </div>
+
+        <div className={style.contactsec}>
+          <div>
+            <h2 style={gfont.style}>Contact Us</h2>
+          </div>
+          <div>
+            <p>Ph : +91 9611346606</p>
+            <br></br>
+            <p>
+              Email:
+              <a style={{color:"#0c2340"}} href="mailto:26abhishek.y@gmail.com">26abhishek.y@gmail.com</a>
+            </p>
+          </div>
+        </div>
+      </div>
+      
+      <div class={style.copyright}>
+        &copy; 2023 Abhishek Designs. All Rights Reserved.
       </div>
     </div>
   );
